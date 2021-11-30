d6a004f9e356cedfc26f84a1198bc82732c974a4
Wilson Huang <window012@hotmail.com>
2021-11-30 13:51:51 +0800

    format string

--- VariadicTemplate/Source.cpp
+++ VariadicTemplate/Source.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <sstream>
 
 template<typename T>
 int sum(const T& first)
@@ -12,12 +14,36 @@ int sum(const T& first, const Rest&... rest)
 	return first + sum(rest...);
 }
 
+template<typename T>
+std::string format(T first)
+{
+	return first;
+}
+
+template<typename T, typename... Rest>
+std::string format(const char *s, T first, Rest... rest)
+{
+	std::stringstream result;
+	for (; *s != '\0'; s++)
+	{
+		if (*s == '{' && *(s + 1) == '}')
+		{
+			result << first;
+			s += 2;
+			break;
+		}
+		else
+		{
+			result << *s;
+		}
+	}
+	result << format(s, rest...);
+	return result.str();
+}
+
 int main()
 {
-	std::cout << sum(1, 2, 3);
-	//sum(1, 2, 3) = 1 + sum(2, 3)
-	//sum(2, 3) = 2 + sum(3) = 5
-	//sum(3) = 3
-	//result => 1+2+3=6
+	std::string s = format("{} {} {}", "hello", 1, 5.2f);
+	std::cout << s;
 	return 0;
 }
